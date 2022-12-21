ddac2cb246d8982fb51f22bb8086224cd75ccdfd
Manuel Duarte  <carlosd99m@gmail.com>
2022-12-21 14:58:34 -0600

    widget navigator (#2)

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_navigator/screens/home_screen.dart';
+import 'package:flutter_navigator/screens/product_screen.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int? productId;
+
+  void setProductId(int id) {
+    setState(() {
+      productId = id;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      theme: CupertinoThemeData(
+    return CupertinoApp(
+      theme: const CupertinoThemeData(
         primaryColor: CupertinoColors.activeBlue,
       ),
-      home: HomeScreen()
+      home: Navigator(
+        pages: [
+          const CupertinoPage(
+            child: HomeScreen(),
+          ),
+          if (productId != null)
+            CupertinoPage(
+              child: ProductScreen(
+                id: productId!,
+              ),
+            ),
+        ],
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            productId = null;
+            return true;
+          }
+          return false;
+        },
+      ),
     );
   }
 }
