a186fd1db0be142898909bd0fe94de38921e08b4
Manuel Duarte  <carlosd99m@gmail.com>
2022-12-22 12:09:55 -0600

    router delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_navigator/app/app.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  setPathUrlStrategy();
+  runApp(const MyApp());
+}
